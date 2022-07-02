ab850807e87226ffa4a7c18baec668d035d65290
Dan Dunai <dunai.dan.a@gmail.com>
2022-07-02 10:18:07 +0300

    Refactoring

--- src/main/java/md/dunai/App.java
+++ src/main/java/md/dunai/App.java
@@ -7,22 +7,23 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class App
 {
+    public static List<Employee> employeeList = new ArrayList<>();
     public static void main( String[] args )
     {
         try {
             Gson gson = new Gson();
             Reader reader = Files.newBufferedReader(Paths.get("employee.json"));
-            List<Employee> employeeList = new Gson().fromJson(reader, new TypeToken<List<Employee>>() {}.getType());
+            employeeList = gson.fromJson(reader, new TypeToken<List<Employee>>() {}.getType());
             for(Employee em : employeeList) {
                 System.out.println(em.getFirstName() + " " + em.getLastName());
             }
 
             reader.close();
-
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } catch (NullPointerException e) {
