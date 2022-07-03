6e6e7c62eec266f7ec6dfb76b340f6668d49beb8
Dan Dunai <dunai.dan.a@gmail.com>
2022-07-03 10:30:19 +0300

    Exported data from database and saved it into a csv file

--- src/main/java/md/dunai/App.java
+++ src/main/java/md/dunai/App.java
@@ -3,14 +3,19 @@ package md.dunai;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVWriter;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App
 {
@@ -62,6 +67,61 @@ public class App
             statement.close();
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
+        }
+
+
+        //get data from database and save into a csv file
+        String name;
+        Map<Integer, String> employees = new HashMap<>();
+        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:supervisor.db")) {
+            Statement statement = conn.createStatement();
+            String query = "SELECT id_employee, firstName, secondName FROM Employee";
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()) {
+                name = resultSet.getString(2) + " " + resultSet.getString(3);
+                employees.put(resultSet.getInt(1), name);
+            }
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        File file = new File("results.csv");
+        try {
+            FileWriter outputfile = new FileWriter(file);
+            CSVWriter writer = new CSVWriter(outputfile);
+
+            String[] header = { "Name", "Total", "Open", "Closed", "In Progress" };
+            writer.writeNext(header);
+
+            for(int id : employees.keySet()) {
+                name = employees.get(id);
+                int open = getTotal(id, Status.OPEN);
+                int closed = getTotal(id, Status.CLOSED);
+                int inProgress = getTotal(id, Status.IN_PROGRESS);
+                int total = open + closed + inProgress;
+                String[] data = {name, Integer.toString(total), Integer.toString(open), Integer.toString(closed), Integer.toString(inProgress)};
+                writer.writeNext(data);
+            }
+            writer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static int getTotal(int employeeID, Status status) {
+        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:supervisor.db")) {
+            Statement statement = conn.createStatement();
+            String query = "SELECT count(id_match) FROM Match WHERE analyst = " + employeeID + " AND Status = '" + status.toString() +"'";
+            ResultSet resultSet = statement.executeQuery(query);
+            int res = resultSet.getInt(1);
+            statement.close();
+            return res;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
